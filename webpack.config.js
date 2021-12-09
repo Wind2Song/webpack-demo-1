0fa649f347583cb0e8658345b20aa934ce60089c
MadarainCong <huangcong0307@163.com>
2021-12-09 16:55:10 +0800

    wds

--- webpack.config.js
+++ webpack.config.js
@@ -2,14 +2,23 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    mode: 'development',
-  entry: './src/index.js',
+  mode: 'development',
+  entry: {index:'./src/index.js'},
+  devtool: 'inline-source-map',
+  devServer: {
+    // static: './dist',
+    // static: {
+    //   directory: path.join('./dist', 'public'),
+    // },
+    // compress: true,
+    // port: 9000,
+  },
   output: {
     filename: '[name].[contenthash].js',
     path: path.resolve(__dirname, 'dist'),
   },
   plugins: [new HtmlWebpackPlugin({
-    title: 'My dd',
+    title: 'Development',
     template: 'src/assets/index.html' // 引用这个路径的模板HTML文件 生成dist HTML文件
   })],
   module: {
