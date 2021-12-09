3c0dbdad0510e1ef9a92c9e5c2efc804dc2ab438
MadarainCong <huangcong0307@163.com>
2021-12-09 11:42:11 +0800

    html引入了css

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     mode: 'development',
@@ -7,4 +8,16 @@ module.exports = {
     filename: '[name].[contenthash].js',
     path: path.resolve(__dirname, 'dist'),
   },
+  plugins: [new HtmlWebpackPlugin({
+    title: 'My dd',
+    template: 'src/assets/index.html' // 引用这个路径的模板HTML文件 生成dist HTML文件
+  })],
+  module: {
+    rules: [
+        {
+          test: /\.css$/,
+          use: [ 'style-loader', 'css-loader' ]
+        },
+    ]
+  }
 };
